3a2cbd5b67aa92653b2e5d99bfe9826394ebb534
adieptr <adie6809@gmail.com>
2023-08-22 18:11:23 +0700

    website ramen

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Toggle class Active
+
+const navbarNav = document.querySelector('.navbar-nav');
+
+// Ketika Menu diklik
+
+document.querySelector('#hamburger-menu').onclick = () => {
+navbarNav.classList.toggle('active');
+};
+
+// Klik di luar sidebar
+
+const hamburger = document.querySelector('#hamburger-menu');
+document.addEventListener ('click', function(e){
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+})
