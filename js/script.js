356a5b9079ca0f607c32397e1c2f6ea0a9fae022
adieptr <e41220989@student.polije.ac.id>
2024-01-16 17:54:36 +0700

    Add cart, modal box, search

--- js/script.js
+++ js/script.js
@@ -1,18 +1,82 @@
-// Toggle class Active
+// Toggle class Active Hamburger menu
 
 const navbarNav = document.querySelector('.navbar-nav');
 
-// Ketika Menu diklik
-
 document.querySelector('#hamburger-menu').onclick = () => {
-navbarNav.classList.toggle('active');
-};
+    navbarNav.classList.toggle('active');
+    };
 
-// Klik di luar sidebar
+
+
+// Toggle class Active shopcart
+
+const shopcart = document.querySelector('.shopping-cart');
+
+document.querySelector('#shopping-cart-button').onclick = () => {
+    shopcart.classList.toggle('active');
+    };
+
+
+
+// Toggle class active untuk search
+
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+}
+
+
+// Klik di luar elemen
 
 const hamburger = document.querySelector('#hamburger-menu');
+const search = document.querySelector('#search-button');
+const shop = document.querySelector('#shopping-cart-button');
+
 document.addEventListener ('click', function(e){
     if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
     }
+
+    if(!search.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
+    }
+
+    if(!shop.contains(e.target) && !shopcart.contains(e.target)) {
+        shopcart.classList.remove('active');
+    }
 })
+
+// Modalbox
+
+const detailmodal = document.querySelector('#item-detail');
+const detailbutton = document.querySelectorAll('.detail-button');
+
+detailbutton.forEach((btn) => {
+    
+    btn.onclick = (e) => {
+        detailmodal.style.display = 'flex';
+        e.preventDefault();
+    };
+
+})
+
+
+
+
+// klik tombol close
+
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    detailmodal.style.display = 'none';
+    e.preventDefault();
+};
+
+// Klik diluar modal
+
+window.onclick = (e) => {
+    if (e.target === detailmodal){
+        detailmodal.style.display = 'none';
+    }
+};
